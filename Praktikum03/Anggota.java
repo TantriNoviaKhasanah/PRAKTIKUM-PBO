b2e4cfae1414d134bb23cf3132b1f765f7d8e754
TantriNoviaKhasanah <tantrinovia157@gmail.com>
2026-09-07 17:47:05 +0700

    Tugas 1

---
+++ Praktikum03/Anggota.java
@@ -0,0 +1,37 @@
+public class Anggota {
+    private String noKTP;
+    private String nama;
+    private int limitPinjaman;
+    private int jumlahPinjaman;
+
+    public Anggota(String noKTP, String nama, int limitPinjaman) {
+        this.noKTP = noKTP;
+        this.nama = nama;
+        this.limitPinjaman = limitPinjaman;
+        this.jumlahPinjaman = 0;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getLimitPinjaman() {
+        return limitPinjaman;
+    }
+
+    public int getJumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void tambahPinjaman(int nominal) {
+        if (jumlahPinjaman + nominal <= limitPinjaman) {
+            jumlahPinjaman += nominal;
+        } else {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        }
+    }
+
+    public void bayarPinjaman(int nominal) {
+        jumlahPinjaman -= nominal;
+    }
+}
